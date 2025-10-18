4fe5e68ac982a9bf252c97f773f62d598eb28d57
Vinicius <vinicius.afonsoalvarez@gmail.com>
2025-10-18 15:14:44 -0300

    htmls pages is just for testing

--- web/app.py
+++ web/app.py
@@ -1,10 +1,65 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify, redirect, url_for
+from datetime import datetime
+
+import sqlite3, os, uuid
+
+from functions import *
+from manip_db import *
 
 app = Flask(__name__)
+app.secret_key = "orcastrator"
 
 @app.route("/")
+def direct():
+    return redirect(url_for("home"))
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+DB_PATH = os.path.join(BASE_DIR, 'database.db') 
+
+#/start-env/{id}
+#/stop-env/{id}
+#/delete-env/{id}
+#/outputfile/{id} -> pagina com o texto de output
+
+#/home -> visualização geral
+@app.route("/home")
 def home():
-    return render_template('home.html')
+    envs = get_envs()
+    return render_template('home.html', envs=envs)
+
+#/new-env -> abre popup com formulario
+@app.route("/new_env")
+def new_env():
+    return render_template('new_env.html')
+
+@app.route("/save_env", methods=['POST'])
+def save_env():
+    #pega os dados do form e cria no database e cria o namespace
+    name = request.form["name"]
+
+    data = {
+        "name": name,
+        "cpu_pins": request.form.get("cpu_pins").split(","),  
+        "cpu_max": int(request.form.get("cpu_max", 0)),
+        "mem": int(request.form.get("mem", 0)),
+        "io": int(request.form.get("io", 0)),
+        "status": 1,
+    }
+    #timestamp atualizado direto no banco
+
+    #adiciona no db
+    env_id = add_env(data)
+    #cria o namespace
+    create_env(env_id, data)
+    
+    files = request.files.getlist("program_file")
+    files = [f for f in files if f.filename]
+    if files:   
+        print('tem programa')
+        add_programs(env_id, name, files)
+
+    return redirect('/home')
 
 if __name__ == "__main__":        
+    init_db()
     app.run(host="0.0.0.0", port=5000, debug=True, use_reloader=True)
---
+++ web/manip_db.py
@@ -0,0 +1,66 @@
+import sqlite3, json, os
+from datetime import datetime
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+DB_PATH = os.path.join(BASE_DIR, 'database.db') 
+
+def init_db():
+    con = sqlite3.connect(DB_PATH)
+    c = con.cursor()
+
+    c.execute("PRAGMA foreign_keys = ON")
+
+    c.execute("CREATE TABLE IF NOT EXISTS envs (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL, cpu_pins TEXT, cpu_max INTEGER, mem INTEGER, io INTEGER, status INTEGER CHECK(status IN (0,1,2)), timestamp DATETIME DEFAULT CURRENT_TIMESTAMP, id_program TEXT)")
+
+    c.execute("CREATE TABLE IF NOT EXISTS programs (id INTEGER PRIMARY KEY AUTOINCREMENT, env_id INTEGER NOT NULL, path TEXT NOT NULL, FOREIGN KEY(env_id) REFERENCES envs(id) ON DELETE CASCADE)")
+
+    con.commit()
+    con.close()
+
+def add_env(data):
+    con = sqlite3.connect(DB_PATH)
+    c = con.cursor()
+
+    c.execute("INSERT INTO envs (name, cpu_pins, cpu_max, mem, io, status) " \
+    "VALUES (?, ?, ?, ?, ?, ?)", (data["name"], str(data.get("cpu_pins")), data.get("cpu_max"), 
+                                     data.get("mem"), data.get("io"), data.get("status")))
+    
+    con.commit()
+    env_id = c.lastrowid
+    con.close()
+
+    return env_id
+
+def add_programs(env_id, name, files):
+    con = sqlite3.connect(DB_PATH)
+    c = con.cursor()
+
+    name = name.lower().replace(" ", "_")
+
+    folder = f"./programs/{name}"
+    os.makedirs(folder, exist_ok=True)
+
+    program_ids = []
+    for file in files:
+        file_path = os.path.join(folder, file.filename.lower().replace(" ", "_"))
+        file.save(file_path)
+
+        c.execute("INSERT INTO programs (env_id, path) VALUES (?, ?)", (env_id, file_path))
+        program_ids.append(c.lastrowid)
+
+    c.execute("UPDATE envs SET id_program=? WHERE id=?", (json.dumps(program_ids), env_id))
+
+    con.commit()
+    con.close()
+    
+def get_envs():
+    con = sqlite3.connect(DB_PATH)
+    c = con.cursor()
+
+    c.execute("SELECT * FROM envs")
+    response = c.fetchall()
+
+    con.close()
+
+    return response
+    
