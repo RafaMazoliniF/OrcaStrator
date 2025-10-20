5d4593b9834639e0b7d3ca3ee161d1baa7233ec6
Vinicius <vinicius.afonsoalvarez@gmail.com>
2025-10-20 16:04:40 -0300

    done?

--- web/manip_db.py
+++ web/manip_db.py
@@ -1,5 +1,6 @@
 import sqlite3, json, os, shutil
 from datetime import datetime
+from functions import *
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 DB_PATH = os.path.join(BASE_DIR, 'database.db') 
@@ -10,10 +11,12 @@ def init_db():
 
     c.execute("PRAGMA foreign_keys = ON")
 
-    c.execute("CREATE TABLE IF NOT EXISTS envs (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL, cpu_pins TEXT, cpu_max INTEGER, mem INTEGER, io INTEGER, status INTEGER CHECK(status IN (0,1,2)), timestamp DATETIME DEFAULT CURRENT_TIMESTAMP, id_program TEXT)")
+    c.execute("CREATE TABLE IF NOT EXISTS envs (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL UNIQUE, cpu_pins TEXT, cpu_max INTEGER, mem INTEGER, io INTEGER, status INTEGER CHECK(status IN (0,1,2)), timestamp DATETIME DEFAULT CURRENT_TIMESTAMP, id_program TEXT)")
 
     c.execute("CREATE TABLE IF NOT EXISTS programs (id INTEGER PRIMARY KEY AUTOINCREMENT, env_id INTEGER NOT NULL, path TEXT NOT NULL, FOREIGN KEY(env_id) REFERENCES envs(id) ON DELETE CASCADE)")
 
+    c.execute("CREATE TRIGGER IF NOT EXISTS prevent_delete_active_env BEFORE DELETE ON envs FOR EACH ROW WHEN OLD.status = 0 BEGIN SELECT RAISE(ABORT, 'Não é permitido deletar um ambiente ativo. Pare-o primeiro'); END;")
+    
     con.commit()
     con.close()
 
@@ -21,15 +24,20 @@ def add_env(data):
     con = sqlite3.connect(DB_PATH)
     c = con.cursor()
 
-    c.execute("INSERT INTO envs (name, cpu_pins, cpu_max, mem, io, status) " \
-    "VALUES (?, ?, ?, ?, ?, ?)", (data["name"], str(data.get("cpu_pins")), data.get("cpu_max"), 
-                                     data.get("mem"), data.get("io"), data.get("status")))
-    
-    con.commit()
-    env_id = c.lastrowid
-    con.close()
+    try:
+        c.execute("INSERT INTO envs (name, cpu_pins, cpu_max, mem, io, status) " \
+        "VALUES (?, ?, ?, ?, ?, ?)", (data["name"], str(data.get("cpu_pins")), data.get("cpu_max"), 
+                                        data.get("mem"), data.get("io"), data.get("status")))
+        
+        con.commit()
+        env_id = c.lastrowid
 
-    return env_id
+        return env_id
+    except sqlite3.IntegrityError:
+        return None
+    
+    finally:
+        con.close()
 
 def add_programs(env_id, name, files):
     con = sqlite3.connect(DB_PATH)
@@ -58,6 +66,8 @@ def start_env(env_id):
     c = con.cursor()
 
     c.execute("UPDATE envs SET status=0 WHERE id=?", (env_id,))
+    
+    #start_env_cgroups()
 
     con.commit()
     con.close()
@@ -68,6 +78,8 @@ def stop_env(env_id):
 
     c.execute("UPDATE envs SET status=1 WHERE id=?", (env_id,))
 
+    #stop_env_cgroups()
+
     con.commit()
     con.close()
 
@@ -77,10 +89,14 @@ def delete_env(env_id):
 
     c.execute("PRAGMA foreign_keys = ON")
 
-    c.execute("DELETE FROM envs WHERE id=?", (env_id,))
-
-    con.commit()
-    con.close()
+    try:
+        c.execute("DELETE FROM envs WHERE id = ?", (env_id,))
+        con.commit()
+        con.close()
+        return None 
+    except sqlite3.IntegrityError as e:
+        con.close()
+        return str(e)
 
 def get_envs():
     con = sqlite3.connect(DB_PATH)
@@ -92,4 +108,21 @@ def get_envs():
     con.close()
 
     return response
-    
+
+def get_files(env_id):
+    con = sqlite3.connect(DB_PATH)
+    c = con.cursor()
+
+    c.execute("PRAGMA foreign_keys = ON")
+
+    c.execute("SELECT path FROM programs WHERE env_id=?", (env_id,))
+    result = c.fetchall()
+
+    con.close()
+
+    if result:
+        paths = [row[0] for row in result]
+        
+        return paths
+    else:
+        return None
